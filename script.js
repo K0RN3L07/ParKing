44b70bd483c43458bf722c62b891c90d7b38fdaa
K0RN3L07 <kornel07.30@gmail.com>
2025-10-25 13:43:22 +0200

    mycommit

---
+++ script.js
@@ -0,0 +1,9 @@
+var mirrored = false;
+
+function toggleLogReg() {
+    var loginCont = document.getElementById("login");
+    var registerCont = document.getElementById("register");
+
+    registerCont.classList.toggle("hide");
+    loginCont.classList.toggle("hide");
+}
