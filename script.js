aa6b51c4aaba9ac6abe71706b29650ae2d01c642
K0RN3L07 <kornel07.30@gmail.com>
2025-11-02 20:37:19 +0100

    login page base

--- script.js
+++
@@ -1,9 +0,0 @@
-var mirrored = false;
-
-function toggleLogReg() {
-    var loginCont = document.getElementById("login");
-    var registerCont = document.getElementById("register");
-
-    registerCont.classList.toggle("hide");
-    loginCont.classList.toggle("hide");
-}
