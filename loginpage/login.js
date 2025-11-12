a5bd2e1d5a8c33dda2fc4c15c779e5f68d9797b2
Matos Kornél <kornel07.30@gmail.com>
2025-11-12 09:52:03 +0100

    login update

---
+++ loginpage/login.js
@@ -0,0 +1,11 @@
+let email = document.getElementById("email");
+let emailLabel = document.getElementById("emailLabel");
+email.addEventListener("input", () => {
+    // if (email.value == "") {
+    //     emailLabel.translate = "(0, -110%)";
+    // }
+    // else {
+    //     emailLabel.translate = "(0, -250%)";
+    // }
+    console.log("asd");
+})
