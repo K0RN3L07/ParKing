bfb9c6ecf4be16369ec420d5ed23038ced0c8a09
Matos Kornél <kornel07.30@gmail.com>
2025-11-12 10:46:34 +0100

    register page

--- loginpage/login.js
+++ loginpage/login.js
@@ -1,11 +1,33 @@
 let email = document.getElementById("email");
 let emailLabel = document.getElementById("emailLabel");
-email.addEventListener("input", () => {
-    // if (email.value == "") {
-    //     emailLabel.translate = "(0, -110%)";
-    // }
-    // else {
-    //     emailLabel.translate = "(0, -250%)";
-    // }
-    console.log("asd");
+
+email.addEventListener("focusin", () => {
+    emailLabel.style.transform = "translateY(-250%)";
+})
+
+email.addEventListener("focusout", () => {
+    if (email.value != "") {
+        emailLabel.style.transform = "translateY(-250%)";
+    }
+
+    else {
+        emailLabel.style.transform = "translateY(-110%)";
+    }
+})
+
+let password = document.getElementById("password");
+let passwordLabel = document.getElementById("passwordLabel");
+
+password.addEventListener("focusin", () => {
+    passwordLabel.style.transform = "translateY(-250%)";
+})
+
+password.addEventListener("focusout", () => {
+    if (password.value != "") {
+        passwordLabel.style.transform = "translateY(-250%)";
+    }
+    
+    else {
+        passwordLabel.style.transform = "translateY(-110%)";
+    }
 })
---
+++ loginpage/register.js
@@ -0,0 +1,67 @@
+let name = document.getElementById("name");
+let nameLabel = document.getElementById("nameLabel");
+
+name.addEventListener("focusin", () => {
+    nameLabel.style.transform = "translateY(-250%)";
+})
+
+name.addEventListener("focusout", () => {
+    if (name.value != "") {
+        nameLabel.style.transform = "translateY(-250%)";
+    }
+
+    else {
+        nameLabel.style.transform = "translateY(-110%)";
+    }
+})
+
+let email = document.getElementById("email");
+let emailLabel = document.getElementById("emailLabel");
+
+email.addEventListener("focusin", () => {
+    emailLabel.style.transform = "translateY(-250%)";
+})
+
+email.addEventListener("focusout", () => {
+    if (email.value != "") {
+        emailLabel.style.transform = "translateY(-250%)";
+    }
+
+    else {
+        emailLabel.style.transform = "translateY(-110%)";
+    }
+})
+
+let password = document.getElementById("password");
+let passwordLabel = document.getElementById("passwordLabel");
+
+password.addEventListener("focusin", () => {
+    passwordLabel.style.transform = "translateY(-250%)";
+})
+
+password.addEventListener("focusout", () => {
+    if (password.value != "") {
+        passwordLabel.style.transform = "translateY(-250%)";
+    }
+    
+    else {
+        passwordLabel.style.transform = "translateY(-110%)";
+    }
+})
+
+let passwordAgain = document.getElementById("passwordAgain");
+let passwordAgainLabel = document.getElementById("passwordAgainLabel");
+
+passwordAgain.addEventListener("focusin", () => {
+    passwordAgainLabel.style.transform = "translateY(-250%)";
+})
+
+passwordAgain.addEventListener("focusout", () => {
+    if (passwordAgain.value != "") {
+        passwordAgainLabel.style.transform = "translateY(-250%)";
+    }
+    
+    else {
+        passwordAgainLabel.style.transform = "translateY(-110%)";
+    }
+})
