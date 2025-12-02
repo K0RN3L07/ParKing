df63c9c96dbbd0f1d11e3af134ea76b1b756eacc
Matos Kornél <kornel07.30@gmail.com>
2025-12-02 10:00:00 +0100

    minor changes

--- main/controllers/mainController.js
+++ main/controllers/mainController.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt');
 const User = require('../models/mainModel');
 
 function getIndex(req, res) {
@@ -25,12 +26,15 @@ async function getUsers(req, res) {
 
 async function registerUser (req, res) {
     const {name, email, phone_num, password} = req.body;
-    const id = await User.registerUser(name, email, phone_num, password);
 
     if (!name || !email || !phone_num || !password){
         return res.status(400).json({msg: 'error'})    
     }
     else{
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+        await User.registerUser(name, email, phone_num, hashedPassword);
+
         res.redirect('/login');
         //return res.status(200).json({message: "Felhasználó hozzáadva!", id});
     }
