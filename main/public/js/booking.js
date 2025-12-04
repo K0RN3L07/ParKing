80b6a8103efcf3f05ac0838508faec54f577f4ce
Matos Kornél <kornel07.30@gmail.com>
2025-12-04 13:49:36 +0100

    finished basics of booking

---
+++ main/public/js/booking.js
@@ -0,0 +1,24 @@
+let radioArray = document.querySelectorAll('input[type="radio"]');
+let deleteSelection = document.getElementById('deleteSelection');
+console.log("asd");
+
+radioArray.forEach(radio => {
+    radio.addEventListener('change', () => {
+        if (radio.checked) {
+            deleteSelection.classList.add("visible");
+        }
+
+        else {
+            if (deleteSelection.classList.contains("visible")) {
+                deleteSelection.classList.remove("visible");
+            }
+        }
+    });
+});
+
+deleteSelection.addEventListener('click', () => {
+    radioArray.forEach(radio => {
+        radio.checked = false;
+    });
+    deleteSelection.classList.remove("visible");
+});
