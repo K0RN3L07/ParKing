0037bc461293c5629ae4cd76647b8494d2bc7233
Matos Kornél <kornel07.30@gmail.com>
2026-01-23 11:05:39 +0100

    created editPassword

--- main/public/js/editProfile.js
+++ main/public/js/editProfile.js
@@ -2,6 +2,7 @@ let editEnabled = false;
 let editBtn = document.getElementById("editBtn");
 let saveBtn = document.getElementById("saveBtn");
 let cancelBtn = document.getElementById("cancelBtn");
+let editPasswordBtn = document.getElementById("editPasswordBtn");
 
 let inputs = document.getElementsByTagName("input");
 
@@ -25,29 +26,10 @@ editBtn.addEventListener("click", () => {
     currentName = name_input.value;
     currentEmail = email_input.value;
     currentPhoneNum = phone_num_input.value;
-
-    if (editEnabled) {
-        for (let i = 0; i < inputs.length; i++) {
-            const input = inputs[i];
-            input.disabled = false;
-        }
-    }
-
-    else {
-        for (let i = 0; i < inputs.length; i++) {
-            const input = inputs[i];
-            input.disabled = true;
-        }
-    }
 });
 
 function setToDefaultView() {
     editEnabled = false;
-
-    for (let i = 0; i < inputs.length; i++) {
-        const input = inputs[i];
-        input.disabled = true;
-    }
 
     editBtn.classList.toggle("hide");
     saveBtn.classList.toggle("hide");
@@ -132,6 +114,103 @@ document.addEventListener("DOMContentLoaded", () => {
         new CreatePopup(msg, success);
         sessionStorage.removeItem("popupMsg");
         sessionStorage.removeItem("popupSuccess");
+    }
+});
+
+
+// Edit Password
+let editPasswordContainer = document.getElementById("editPasswordContainer");
+let cancelPasswordBtn = document.getElementById("cancelPasswordBtn");
+let editPasswordForm = document.getElementById("editPasswordForm");
+
+editPasswordBtn.addEventListener("click", () => {
+    if (!editPasswordContainer.classList.contains("showBackgroundFilter")) {
+        editPasswordContainer.classList.add("showBackgroundFilter");
+    }
+});
+
+cancelPasswordBtn.addEventListener("click", () => {
+    if (editPasswordContainer.classList.contains("showBackgroundFilter")) {
+        editPasswordContainer.classList.remove("showBackgroundFilter")
+    }
+});
+
+editPasswordForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let currentPassword = document.getElementById("currentPassword");
+    let newPassword = document.getElementById("newPassword");
+    let newPasswordAgain = document.getElementById("newPasswordAgain");
+
+    //#region Password Regex
+
+    // Minimum length (8 characters)
+    const lengthRegex = /^.{8,}$/;
+
+    // At least one uppercase letter
+    const uppercaseRegex = /[A-Z]/;
+
+    // At least one lowercase letter
+    const lowercaseRegex = /[a-z]/;
+
+    // At least one number
+    const numberRegex = /\d/;
+
+    // At least one special character
+    const specialCharRegex = /[^A-Za-z0-9]/;
+
+    // No space character
+    const noSpaceRegex = /^\S+$/;
+
+    // Hungarian character regex
+    const hunCharRegex = /[áéíóöőúüűÁÉÍÓÖŐÚÜŰ]/;
+
+    if (!uppercaseRegex.test(newPassword.value)) {
+        new CreatePopup("A jelszónak tartalmaznia kell legalább 1 nagybetűs karakter!", false);
+        return;
+    }
+
+    if (!lowercaseRegex.test(newPassword.value)) {
+        new CreatePopup("A jelszónak tartalmaznia kell legalább 1 kisbetűs karakter!", false);
+        return;
+    }
+
+    if (!numberRegex.test(newPassword.value)) {
+        new CreatePopup("A jelszónak tartalmaznia kell legalább 1 számot!", false);
+        return;
+    }
+
+    if (!specialCharRegex.test(newPassword.value)) {
+        new CreatePopup("A jelszónak tartalmaznia kell legalább 1 speciális karakter!", false);
+        return;
+    }
+
+    if (!noSpaceRegex.test(newPassword.value)) {
+        new CreatePopup("A jelszó nem tartalmazhat szóközt!", false);
+        return;
+    }
+
+    if (hunCharRegex.test(newPassword.value)) {
+        new CreatePopup("A jelszó nem tartalmazhat ékezetes karaktereket!", false);
+        return;
+    }
+
+    if (!lengthRegex.test(newPassword.value)) {
+        new CreatePopup("A jelszónak legalább 8 karakter hosszúnak kell lennie!", false);
+        return;
+    }
+    //#endregion
+
+    if (newPassword.value !== newPasswordAgain.value) {
+        new CreatePopup("A két jelszó nem egyezik!", false);
+        return;
+    }
+
+    // Fetch goes here 🔽
+    
+
+    if (editPasswordContainer.classList.contains("showBackgroundFilter")) {
+        editPasswordContainer.classList.remove("showBackgroundFilter")
     }
 });
 
