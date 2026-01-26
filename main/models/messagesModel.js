549d852ed54e2581f66a0d6b746f076fa6da03a8
TapiDavid <tapolcsanyidavid@gmail.com>
2026-01-26 11:09:55 +0100

    fixed sendMessage

--- main/models/messagesModel.js
+++ main/models/messagesModel.js
@@ -3,7 +3,7 @@ const db = require('../config/database');
 exports.sendMessage = (user_id, message) => {
     return new Promise((resolve, reject) => {
         db.query(
-            "INSERT INTO messages ('user_id', 'message') VALUES (?, ?)",
+            "INSERT INTO messages (user_id, message) VALUES (?, ?)",
             [user_id, message],
             (err, result) => {
                 if (err) return reject(err);
