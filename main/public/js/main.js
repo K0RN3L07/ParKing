cf1ae1406c036a442c337fabba1f59800702b1cc
TapiDavid <tapolcsanyidavid@gmail.com>
2026-01-26 12:05:26 +0100

    working sendMessage

--- main/public/js/main.js
+++ main/public/js/main.js
@@ -2,3 +2,27 @@ function deleteMessageContent() {
     let messageInput = document.getElementById("messageInput");
     messageInput.value = "";
 }
+
+document.getElementById("sendMessageForm").addEventListener("submit", async (e) => {
+    e.preventDefault()
+    
+    let data = document.getElementById("messageInput").value;
+
+    try {
+        const res = await fetch("/sendMessage", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ message: data })
+        });
+    
+        const result = await res.json();
+    
+        new CreatePopup(result.msg, result.success);
+        deleteMessageContent();
+    
+    } catch (err) {
+        console.error(err);
+        new CreatePopup("Szerverhiba történt!", false);
+    }
+
+})
