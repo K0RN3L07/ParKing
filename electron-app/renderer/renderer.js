a97a5d0def314f451db30b83190e51d9388f26b0
Matos Kornél <kornel07.30@gmail.com>
2026-03-05 13:05:03 +0100

    updated electron app

--- electron-app/renderer/renderer.js
+++ electron-app/renderer/renderer.js
@@ -8,6 +8,14 @@ async function loadUsers() {
     console.log(users);
 
     users.forEach(user => {
+        const date = new Date(user.registered_at);
+
+        const formattedDate =
+            date.getFullYear() + ". " +
+            String(date.getMonth() + 1).padStart(2, "0") + ". " +
+            String(date.getDate()).padStart(2, "0") + ". " +
+            String(date.getHours()).padStart(2, "0") + ":" +
+            String(date.getMinutes()).padStart(2, "0");
 
         const row = document.createElement("tr");
 
@@ -16,8 +24,8 @@ async function loadUsers() {
             <td>${user.name}</td>
             <td>${user.email}</td>
             <td>${user.phone_num}</td>
-            <td>${user.password}</td>
-            <td>${user.registered_at}</td>
+            <td class='text-truncate' style='max-width:100px;'>${user.password}</td>
+            <td>${formattedDate}</td>
             <td class='text-center'><button class='btn btn-primary'><i class="bi bi-pencil-fill"></i></button></td>
             <td class='text-center'><button class='btn btn-danger'><i class="bi bi-trash3-fill"></i></button></td>
         `;
@@ -27,3 +35,7 @@ async function loadUsers() {
 }
 
 loadUsers();
+
+document.getElementById("refreshButton").addEventListener("click", () => {
+    window.location.reload();
+}) 
